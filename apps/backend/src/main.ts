99b37864ea8915070af3e2b2f7045ebe3ce276fb
nhuipawarit <pawarit.c@evme.io>
2025-05-24 18:32:02 +0700

    refactor: clean up code formatting and structure across multiple files; add global API prefix in main.ts and introduce mock data for products

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+
+void bootstrap();
