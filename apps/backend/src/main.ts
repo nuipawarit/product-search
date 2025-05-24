592f35b0d47763713ca7552acc8c1e034a3c69d9
nhuipawarit <pawarit.c@evme.io>
2025-05-25 02:53:08 +0700

    feat(backend): add Dockerfile for containerization and enable CORS in main.ts based on frontend URL

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,8 +4,21 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const frontendUrl = process.env.FRONTEND_URL;
+  if (frontendUrl) {
+    app.enableCors({
+      origin: frontendUrl,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  }
+
+  const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port, () => {
+    console.log(`NestJS server is running on port ${port}`);
+  });
 }
 
 void bootstrap();
